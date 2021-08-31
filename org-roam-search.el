d0762f48dd7bffe1ae2fde39bc641adf854c49f8
savnkk_manjaro <39103342+natask@users.noreply.github.com>
2021-08-31 04:16:16 -0700

    (feat): good enough to replace org-roam-find-file

--- org-roam-search.el
+++ org-roam-search.el
@@ -7,9 +7,9 @@
 ;; Created: August 30, 2021
 ;; Modified: August 30, 2021
 ;; Version: 0.0.1
-;; Keywords: org-roam
+;; Keywords: matching,org-roam
 ;; Homepage: https://github.com/savnkk/org-roam-search
-;; Package-Requires: ((emacs "26.1") (org-roam "1.2.3"))
+;; Package-Requires: ((emacs "26.1") (org-roam "1.2.3") (sexp-string "0.0.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -76,7 +76,7 @@
             (rec `(titles ,@rest) accum))))
     (query :name query
            :transform
-           (((pred stringp) (concat "%%" element "%%")))
+           (((pred stringp) (intern (concat "%%" element "%%"))))
            :search
            (((pred stringp) element))
            :stringify
@@ -95,15 +95,13 @@
 (fset 'org-roam-search--stringify-query (sexp-string--define-transform-query-fn "org-roam-search" :stringify))
 
 (defun org-roam-search--get-title-path-completions (conditions)
-  "Return an alist for completion.
+  "Return an alist for completion on CONDITIONS.
 The car is the displayed title for completion, and the cdr is a
 plist containing the path and title for the file."
-  (let* ((rows (org-roam-db-query `[:select [files:file titles:title tags:tags files:meta] :from titles
-                                    :left :join tags
-                                    :on (= titles:file tags:file)
-                                    :left :join files
-                                    :on (= titles:file files:file)
-                                    :where ,conditions]))
+  (let* ((rows (delve-db-safe-query `[:select [files:file titles:title tags:tags files:meta] :from titles
+                                      :left :join files :using [[ file ]]
+                                      :left :join tags :using  [[ file ]]
+                                      :where ,conditions]))
          completions)
     (setq rows (seq-sort-by (lambda (x)
                               (plist-get (nth 3 x) :mtime))
@@ -121,14 +119,54 @@ SOURCE is not used."
   (let ((prefix (propertize "[?] "
                             'face 'helm-ff-prefix)))
     (cons (propertize helm-pattern
-                      'display (concat prefix (--> helm-pattern
-                                                   (org-roam-search--query-string-to-sexp it)
-                                                   (org-roam-search--stringify-query it)
-                                                   (org-roam--add-tag-string
-                                                    (combine-and-quote-strings
-                                                     (plist-get it :title))
-                                                    (plist-get it :tags))))
-    candidates)))
+                      'display (concat prefix (condition-case nil (--> helm-pattern
+                                                                       (org-roam-search--query-string-to-sexp it)
+                                                                       (org-roam-search--stringify-query it)
+                                                                       (org-roam--add-tag-string
+                                                                        (combine-and-quote-strings
+                                                                         (plist-get it :title))
+                                                                        (plist-get it :tags)))
+                                                (error ""))))
+          candidates)))
+
+(cl-defun org-roam-search-completion--completing-read (prompt choices &key
+                                                              require-match initial-input
+                                                              action)
+  "Present a PROMPT with CHOICES and optional INITIAL-INPUT.
+If REQUIRE-MATCH is t, the user must select one of the CHOICES.
+Return user choice."
+  (let ((res (let ((source (helm-make-source prompt 'helm-source-sync
+                             :candidates (lambda ()
+                                           (condition-case nil
+                                               (--> helm-pattern
+                                                    (org-roam-search--query-string-to-sexp it)
+                                                    (org-roam-search--transform-query it)
+                                                    (org-roam-search--get-title-path-completions it))
+                                             (error
+                                              (mapcar #'car choices))))
+                             :match #'identity
+                             :fuzzy-match nil
+                             :multimatch nil
+                             :nohighlight t
+                             :volatile t
+                             :filtered-candidate-transformer
+                             (and (not require-match)
+                                  #'org-roam-search-completion--helm-candidate-transformer)))
+                   (buf (concat "*org-roam-search "
+                                (s-downcase (s-chop-suffix ":" (s-trim prompt)))
+                                "*")))
+               (or (helm :sources source
+                         :action (if action
+                                     (prog1 action
+                                       (setq action nil))
+                                   #'identity)
+                         :prompt prompt
+                         :input initial-input
+                         :buffer buf)
+                   (keyboard-quit)))))
+    (if action
+        (funcall action res)
+      res)))
 
 (defun org-roam-search-find-file (&optional initial-prompt completions filter-fn no-confirm)
   "Find and open an Org-roam file.
@@ -142,21 +180,39 @@ If NO-CONFIRM, assume that the user does not want to modify the initial prompt."
   (interactive)
   (unless org-roam-mode (org-roam-mode))
   (let* ((completions (funcall (or filter-fn #'identity)
-                               (or completions (org-roam-search--get-title-path-completions ))))
-         (title-with-tags (if no-confirm
+                               (or completions (org-roam--get-title-path-completions))))
+         (res (if no-confirm
                               initial-prompt
-                            (org-roam-completion--completing-read "File: " completions
-                                                                  :initial-input initial-prompt)))
+                            (org-roam-search-completion--completing-read "File: " completions
+                                                                         :initial-input initial-prompt)))
 
-         (res (cdr (assoc title-with-tags completions)))
          (file-path (plist-get res :path)))
     (if file-path
         (org-roam--find-file file-path)
-      (let ((org-roam-capture--info `((title . ,title-with-tags)
-                                      (slug  . ,(funcall org-roam-title-to-slug-function title-with-tags))))
-            (org-roam-capture--context 'title))
-        (setq org-roam-capture-additional-template-props (list :finalize 'find-file))
-        (org-roam-capture--capture)))))
+      (when-let* ((title-tags-plist (-some->> res
+                                      (org-roam-search--query-string-to-sexp)
+                                      (org-roam-search--stringify-query)))
+                  (title (combine-and-quote-strings (plist-get title-tags-plist :title)))
+                  (tags (let ((tags (plist-get title-tags-plist :tags)))
+                          (if (null tags) ""
+                            (format ":%s:" (mapconcat #'identity tags ":")))))
+                  (org-roam-capture--info `((title . ,title)
+                                            (slug  . ,(funcall org-roam-title-to-slug-function title))))
+                  (org-roam-capture--context 'title)
+                  (org-capture-link-is-already-stored 't)
+                  (org-store-link-plist (list :description title :tags tags))
+                  (org-roam-capture-additional-template-props (list :finalize 'find-file))
+                  (org-roam-capture-templates '(("l" "from link" plain (function org-roam--capture-get-point)
+                                                 "%?"
+                                                 :file-name "%:description"
+                                                 :head "#+title: %:description
+#+roam_alias:
+#+filetags: %:tags
+#+roam_keys: %:link
+#+created: %U
+#+last_modified: %U \n"
+                                                 :unnarrowed t))))
+        (org-roam-capture--capture 'nil "l")))))
 
 (provide 'org-roam-search)
 ;;; org-roam-search.el ends here
